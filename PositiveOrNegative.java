5e886590d63d56cc7fa6fe226a349f466bd26a61
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2018-09-03 17:26:17 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,20 @@
+package guvi;
+import java.util.*;
+public class PositiveOrNegative {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n<0) {
+	System.out.println(n+" value  is negative");
+}
+else if(n>0) {
+	System.out.println(n+"the value is  positive");
+}
+else 
+{
+	System.out.println("the value is zero");
+}
+	}
+
+}
