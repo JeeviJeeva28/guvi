21acda7ff110134ad12ecf47d23b413d789bbfef
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-03-08 16:59:17 +0530

    Create ArrayIndex.java

---
+++ ArrayIndex.java
@@ -0,0 +1,24 @@
+
+package guvi;
+import java.util.*;
+public class Median {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int sum=0;
+		int median=0;
+		 int n=sc.nextInt();
+		int a[]=new int[n];
+	       for(int i=0;i<n;i++){
+	          a[i]=sc.nextInt();
+	       }
+	       for(int i=0;i<n;i++){
+	    	   System.out.print(a[i]+" ");
+	      System.out.println(i);
+	      
+	}
+	       
+	}
+}
+
+      
