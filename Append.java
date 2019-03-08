a2f4a081531265373767bb53e1dabf26bab42c97
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-03-08 17:41:35 +0530

    Create Append.java

---
+++ Append.java
@@ -0,0 +1,11 @@
+package guvi;
+import java.util.*;
+public class Median {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+	StringBuilder sb=new StringBuilder(s);
+	System.out.println(sb.append("."));
+}
+}
