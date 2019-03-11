047abcb0b1166d131bf7a2f10776279edf5e1488
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-03-11 18:18:07 +0530

    Create CountNumberWords.java

---
+++ CountNumberWords.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int count=0;
+    for(int i=0;i<s.length();i++){
+    char ch=s.charAt(i);
+    if(ch==' ' && ch+1!=' '){
+        count++;
+    }
+    }
+        
+  System.out.println(count);    
+    }
+}
