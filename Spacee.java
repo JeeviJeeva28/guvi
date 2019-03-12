a5117b426e09c2f157f4384d17731f1978243711
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-03-12 17:12:46 +0530

    Rename Space.java to Spacee.java

---
+++ Spacee.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     int a[]=new int[n];
+     for(int i=0;i<a.length;i++){
+         a[i]=sc.nextInt();
+     }
+     for(int i=0;i<a.length;i++){
+         System.out.print(a[i]+" ");
+     }
+    }
+}
