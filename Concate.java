c9b4e81993ae19c2d6af11e5ffcb6e6a03d8791c
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-03-20 18:43:57 +0530

    Create Concate.java

---
+++ Concate.java
@@ -0,0 +1,11 @@
+package javavaa;
+import java.util.*;
+public class Num {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+String s1=sc.next();
+System.out.println(s+s1);
+	}            
+}
