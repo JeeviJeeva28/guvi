4a0063f46cc31113f2326de9609c2fc21054fea9
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-03-20 18:39:48 +0530

    Create Equal.java

---
+++ Equal.java
@@ -0,0 +1,17 @@
+package javavaa;
+import java.util.*;
+public class Num {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+String s1=sc.next();
+if(s.length()>s1.length()) {
+	System.out.println(s);
+}
+else if(s.length()==s1.length()){
+	System.out.println(s);
+}else
+	System.out.println(s1);
+	}            
+}
