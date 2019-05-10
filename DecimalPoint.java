19ecdf89a218a09285ed1b715f1d8399093087c6
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-05-10 11:35:37 +0530

    Create DecimalPoint.java

---
+++ DecimalPoint.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+float a=sc.nextFloat();
+System.out.print(Math.ceil(a));
+          }
+      }
+    
