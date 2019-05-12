55f869387edca28a116618aadf787191bb069939
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-05-12 15:33:07 +0530

    Create Rangeee.java

---
+++ Rangeee.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+    for(int i=0;i<=10;i++){
+        if(i==n){
+            System.out.println("yes");
+        }
+        else
+        System.out.println("no");
+        break;
+        
+    }
+    
+}
+}
