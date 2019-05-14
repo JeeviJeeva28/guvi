0211656bd29130cf269bfc04c377f906d940ef01
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-05-14 21:24:44 +0530

    Create CouchSum.java

---
+++ CouchSum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+       int n1=sc.nextInt();
+       System.out.println(Math.abs(n-n1));
+      
+    }
+}
