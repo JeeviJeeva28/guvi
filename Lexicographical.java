c6a63758bd0a3cc0513c0a772d7b89f65d9e7739
JeeviJeeva28 <42933462+JeeviJeeva28@users.noreply.github.com>
2019-06-10 15:58:03 +0530

    Create Lexicographical.java

---
+++ Lexicographical.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+public class Fancy {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   Scanner sc=new Scanner(System.in);
+   String s=sc.next();
+char ch[]=s.toCharArray();
+Arrays.sort(ch);
+System.out.println(ch);
+  
+	}
+
+}
+
